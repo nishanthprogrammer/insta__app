af29b8b6d96ab48a66c04b2988711afc965393d0
nishanthprogrammer <nishanthprogrammer18@gmail.com>
2025-07-05 12:33:45 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import json
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "HELLO USER"
+
+@app.route("/privacy_policy")
+def privacy_policy():
+    with open("D:\SDE\INSTAGRAM AUTOMATION PROJECT\insta_app\privacy_policy.html","rb") as file:
+        privacy_policy_html = file.read()
+        return privacy_policy_html
+    
+
+@app.route("/webhook",methods=['GET','POST'])
+def webhook():
+    if request.method=='POST':
+        try:
+            print(request.get_json(),indent=2)
+        except:
+            pass
+        return "This is a POST Request, Hello Webhook!"
+    elif request.method=='GET':
+        hub_mode = request.args.get("hub.mode")
+        hub_challenge = request.args.get("hub.challenge")
+        hub_verify_token = request.args.get("hub.verify_token")
+        if hub_challenge:
+            return hub_challenge
+        else:
+            return "This is a GET REQUEST"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
